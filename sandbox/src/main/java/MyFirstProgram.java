fcaafe3222adcf0d79c91972a38123c0d22d8359
PotapovMS <Potapov_MS@outlook.com>
2016-12-11 20:39:20 +0300

    Создан проеет для Idea

--- sandbox/src/main/java/MyFirstProgram.java
+++ sandbox/src/main/java/MyFirstProgram.java
@@ -1,8 +1,8 @@
-public class MyFirstProgram{
+public class MyFirstProgram {
 
-public static void main (String[] args){
+    public static void main(String[] args) {
 
-System.out.println("Hello, world!");
-}
+        System.out.println("Hello, world!");
+    }
 
 }
