43fca3a35a4c5769f4f13ef4a831edc3c8065834
PotapovMS <Potapov_MS@outlook.com>
2016-12-13 22:45:46 +0300

    Создан тест для создания групп

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Square.java
@@ -0,0 +1,18 @@
+package ru.stqa.pft.sandbox;
+
+/**
+ * Created by Potap on 13.12.2016.
+ */
+public class Square {
+  public double a;
+
+  public Square(double a){
+    this.a = a;
+  }
+
+  public double area(){
+    return a*a;
+  }
+
+
+}
